44c2a816226325f768669f4cf9ec06a2396e7762
Luanzeixon <luananjos765@gmail.com>
2022-01-17 12:58:48 -0300

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeuPrimeiroComponent,
+    MeuPrimeiro2Component
   ],
   imports: [
     BrowserModule,
